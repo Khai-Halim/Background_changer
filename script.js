b3305071798e43da53d3644639331192f69d3e8b
Khai <Khai-Halim@users.noreply.github.com>
2020-03-09 11:09:40 +1000

    Changed backgrounds to pictures instead of colors

--- script.js
+++ script.js
@@ -2,21 +2,23 @@
 let btn = document.querySelector('#btn-change-background');
 let body = document.querySelector('body');
 
-//Array of background colors to change to
-let colors = ['blue', 'green', 'yellow', 'red', 'purple'];
-let currentColorIndex;
+//Array of backgrounds to change to
+let backgrounds = ['url(/img/pic_1.jpg)','url(/img/pic_2.jpg)','url(/img/pic_3.jpg)','url(/img/pic_4.jpg)','url(/img/pic_5.jpg)'];
+let currentImageIndex;
 
-//change color on clicking the button
+//change background on clicking the button
 btn.addEventListener('click', function(){
     //picks a random number
-    let random = Math.floor(Math.random()*colors.length);
+    let random = Math.floor(Math.random()*backgrounds.length);
 
   //Stop the color from repeating
-    while(random == currentColorIndex){
-        random = Math.floor(Math.random()*colors.length);
-        //console.log('color is the same as previous');
+    while(random == currentImageIndex){
+        random = Math.floor(Math.random()*backgrounds.length);
+        //console.log('background has repeated');
     }
-    currentColorIndex = random;
-    //changes background color
-    body.style.background = colors[random];
+    currentImageIndex = random;
+    body.style.background = backgrounds[random];
+    body.style.backgroundSize = 'cover';
+    body.style.backgroundPositionY = 'center';
+    body.style.backgroundrepeat = 'none';
 });
