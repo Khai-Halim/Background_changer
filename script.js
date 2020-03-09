cb02ace694023e4b2019e9cb1e2df2c5021bd8dc
Khai <Khai-Halim@users.noreply.github.com>
2020-03-09 10:05:32 +1000

    fixed issue with backgrounds repeating

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
+//DOM variables
 let btn = document.querySelector('#btn-change-background');
 let body = document.querySelector('body');
 
-//background colors to change to
+//Array of background colors to change to
 let colors = ['blue', 'green', 'yellow', 'red', 'purple'];
+let currentColorIndex;
 
 //change color on clicking the button
 btn.addEventListener('click', function(){
+    //picks a random number
     let random = Math.floor(Math.random()*colors.length);
+
+  //Stop the color from repeating
+    while(random == currentColorIndex){
+        random = Math.floor(Math.random()*colors.length);
+        //console.log('color is the same as previous');
+    }
+    currentColorIndex = random;
+    //changes background color
     body.style.background = colors[random];
-    console.log(random);
 });
